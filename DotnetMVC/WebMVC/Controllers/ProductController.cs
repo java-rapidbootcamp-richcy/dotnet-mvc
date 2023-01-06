ed94ce32a66b8e74ee4bbf8708b157e81d2889ad
Richcy Dian Sukma <richcyveliz@gmail.com>
2023-01-06 14:12:23 +0700

    Checkpoint Study: 14:00

---
+++ DotnetMVC/WebMVC/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Add()
+		{
+			return View();
+		}
+		public IActionResult List()
+		{
+			List<ProductViewModel> productList = new List<ProductViewModel>()
+			{
+				new ProductViewModel(1, "Mie Kuah Kari Ayam", "Makanan", 2500),
+				new ProductViewModel(2, "Mie Kuah Soto", "Makanan", 2600),
+				new ProductViewModel(3, "Mie Kuah Ayam Bawang", "Makanan", 2300),
+				new ProductViewModel(4, "Jus Jeruk", "Minumam", 5000),
+
+			};
+			return View(productList);
+		}
+
+		public IActionResult Details()
+		{
+			ProductViewModel product = new ProductViewModel(4, "Jus Jeruk", "Minuman", 5000);
+			return View(product);
+		}
+	}
+}
